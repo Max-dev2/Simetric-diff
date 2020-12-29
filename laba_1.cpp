d0b498a050ec5812172921a8eb49845e1e1a4406
Maks-dev2 <maxym2003f@gmail.com>
2020-12-29 15:10:36 +0200

    Second commit

--- laba_1.cpp
+++ laba_1.cpp
@@ -5,11 +5,14 @@ int main(){
     cout<<"YEar";
     int n=5;
     int A[n];
+    int B[10]={10,9,8,7,6,5,4,3,2,1};
     for (int i = 0; i < n; i++)
         cin>>A[i];
     for (int i =0 ; i < n;i++)
         cout<<A[i]<<" ";
         cout<<"\n Happy New Year"<<endl ;   
+    for (int i = 0 ; i < 10 ; i++)
+        cout<<B[i];    
     system("pause");      
    return 0;
 }
